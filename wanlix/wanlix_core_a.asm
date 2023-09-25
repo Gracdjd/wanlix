15976a42690328b1ba2b7df0729cfe7000365026
WZ <2072401390@qq.com>
2023-09-25 16:40:28 +0800

    contextswitch

--- wanlix/wanlix_core_a.asm
+++ wanlix/wanlix_core_a.asm
@@ -1,5 +1,21 @@
     INCLUDE wanlix_core_a
 
+WLX_ContextSwitch
+
+    ;保存当前任务的栈信息
+    STMIA R0!, {R4 - R14} ;保存R4-R14
+    MRS R2, XPSR    ;获取XPSR
+    STMIA R0, {R2}          ;保存到寄存器组中的XPSR
+
+    LDMIA R1! {R4 - R13};
+    ADD R1, #4
+    LDMIA R1, {R0}
+    MSR XPSR, R0
+    SUB R1, #4
+    LDMIA R1, {PC}  ;切换任务
+
+
+    
 WLX_SwitchToTask
 
     ;恢复将要运行的栈任务并运行新任务
